bcdb31746d140ec7d0f3b23f11857e6cf0542aec
lukasapolenar <lukas.apolenar@gmail.com>
2026-07-16 17:47:02 +0200

    Create runtime-config.js

---
+++ outputs/runtime-config.js
@@ -0,0 +1,5 @@
+globalThis.__OKRAJOVKA_CONFIG__ = {
+  supabaseUrl: "https://yyckrkcoqcpuxjgwelde.supabase.co",
+  supabaseAnonKey: "sb_publishable_eRWGH1ON0I3QDwk1dW4LtQ_D9FGEtnE",
+  appUrl: ""
+};
